53cc607e72e2a5a70910aa9e73c76bd601d8ac91
nik <volsknik@gmail.com>
2020-12-25 16:24:41 +0200

    fix(trycatcher): unresolved Promise replace to try/catch

--- src/components/User/tryCatcher.js
+++ src/components/User/tryCatcher.js
@@ -13,99 +13,99 @@ const catcher = {
     },
     findById(fn) {
         return (req, res, next) => {
-            new Promise(() => {
+            try {
                 const { error } = UserValidation.findById(req.params);
                 if (error) {
                     throw new ValidationError(error.details);
                 }
-            })
-                .then(fn(req, res, next))
-                .catch((error) => {
-                    if (error instanceof ValidationError) {
-                        return res.status(422).json({
-                            error: error.name,
-                            details: error.message,
-                        });
-                    }
-                    res.status(500).json({
-                        message: error.name,
+
+                return fn(req, res, next);
+            } catch (error) {
+                if (error instanceof ValidationError) {
+                    return res.status(422).json({
+                        error: error.name,
                         details: error.message,
                     });
-                    return next(error);
+                }
+                res.status(500).json({
+                    message: error.name,
+                    details: error.message,
                 });
+                return next(error);
+            }
         };
     },
     create(fn) {
         return (req, res, next) => {
-            new Promise(() => {
+            try {
                 const { error } = UserValidation.create(req.body);
                 if (error) {
                     throw new ValidationError(error.details);
                 }
-            })
-                .then(fn(req, res, next))
-                .catch((error) => {
-                    if (error instanceof ValidationError) {
-                        return res.status(422).json({
-                            message: error.name,
-                            details: error.message,
-                        });
-                    }
-                    res.status(500).json({
+
+                return fn(req, res, next);
+            } catch (error) {
+                if (error instanceof ValidationError) {
+                    return res.status(422).json({
                         message: error.name,
                         details: error.message,
                     });
-                    return next(error);
+                }
+                res.status(500).json({
+                    message: error.name,
+                    details: error.message,
                 });
+                return next(error);
+            }
         };
     },
     updateById(fn) {
         return (req, res, next) => {
-            new Promise(() => {
+            try {
                 const { error } = UserValidation.updateById(req.body);
                 if (error) {
                     throw new ValidationError(error.details);
                 }
-            })
-                .then(fn(req, res, next))
-                .catch((error) => {
-                    if (error instanceof ValidationError) {
-                        return res.status(422).json({
-                            message: error.name,
-                            details: error.message,
-                        });
-                    }
-                    res.status(500).json({
+
+                return fn(req, res, next);
+            } catch (error) {
+                if (error instanceof ValidationError) {
+                    return res.status(422).json({
                         message: error.name,
                         details: error.message,
                     });
-                    return next(error);
+                }
+                res.status(500).json({
+                    message: error.name,
+                    details: error.message,
                 });
+                return next(error);
+            }
         };
     },
     deleteById(fn) {
         return (req, res, next) => {
-            new Promise(() => {
+            try {
                 const { error } = UserValidation.deleteById(req.body);
 
                 if (error) {
                     throw new ValidationError(error.details);
                 }
-            })
-                .then(fn(req, res, next))
-                .catch((error) => {
-                    if (error instanceof ValidationError) {
-                        return res.status(422).json({
-                            message: error.name,
-                            details: error.message,
-                        });
-                    }
-                    res.status(500).json({
+
+                return fn(req, res, next);
+            } catch (error) {
+                if (error instanceof ValidationError) {
+                    return res.status(422).json({
                         message: error.name,
                         details: error.message,
                     });
-                    return next(error);
+                }
+                res.status(500).json({
+                    message: error.name,
+                    details: error.message,
                 });
+                return next(error);
+            }
         };
     },
 };
