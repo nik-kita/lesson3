246ff442128f90abecd1a02948546a4ead441ad6
nik <volsknik@gmail.com>
2020-12-25 12:48:57 +0200

    feat: tryCatcher

---
+++ src/components/User/tryCatcher.js
@@ -0,0 +1,52 @@
+const ValidationError = require('../../error/ValidationError');
+const UserValidation = require('./validation');
+
+const catcher = {
+    findAll(fn) {
+        return (req, res, next) => fn(req, res, next).catch((error) => {
+            res.status(500).json({
+                error: error.message,
+                details: null,
+            });
+            next(error);
+        });
+    },
+    findById(fn) {
+        return (req, res, next) => fn(req, res, next).catch((error) => {
+            if (error instanceof ValidationError) {
+                return res.status(422).json({
+                    error: error.name,
+                    details: error.message,
+                });
+            }
+            res.status(500).json({
+                message: error.name,
+                details: error.message,
+            });
+            return next(error);
+        });
+    },
+    create(fn) {
+        return (req, res, next) => fn(req, res, next).catch((error) => {
+            if (error instanceof ValidationError) {
+                return res.status(422).json({
+                    message: error.name,
+                    details: error.message,
+                });
+            }
+            res.status(500).json({
+                message: error.name,
+                details: error.message,
+            });
+            return next(error);
+        });
+    },
+    updateById(fn) {
+        return this.create(fn);
+    },
+    deleteById(fn) {
+        return this.create(fn);
+    },
+};
+
+module.exports = (fn) => catcher[fn.name](fn);
